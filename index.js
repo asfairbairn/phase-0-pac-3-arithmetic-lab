0830274b41ac8245e2d5e00820709be759d953e4
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-10-19 11:18:54 -0500

    commit tree ab444c44655cb4886494a75dc22d3a32a81c6e3a

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return(a + b);
+}
+
+function subtract(a, b) {
+    return(a - b);
+}
+
+function multiply(a, b) {
+    return(a * b);
+}
+
+function divide(a, b) {
+    return(a / b)
+}
+
+function increment(n) {
+    return(n + 1)
+}
+
+function decrement(n) {
+    return(n - 1)
+}
+function makeInt(n, r = 10) {
+    return parseInt(n, r)
+}
+function preserveDecimal(n, r = 10) {
+    return parseFloat(n, r)
+}
